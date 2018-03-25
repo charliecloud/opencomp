d153ef271233d14689fe9d9eb9ab4605281afb23
Charles Cloud <charliecloud@gmail.com>
2018-03-24 22:33:02 -0400

    Add all locations and change format to city and state

--- models/location.rb
+++ models/location.rb
@@ -2,14 +2,20 @@ class Location
   include DataMapper::Resource
 
   property :id, Serial
-  property :name, String
+  property :city, String
+  property :state, String
+  property :population, Integer
+  property :rank, Integer
   property :created_at, DateTime
   property :deleted, Boolean, :default => false
 
   def to_json
     {
         "id" => @id,
-        "name" => @name,
+        "city" => @city,
+        "state" => @state,
+        "population" => @population.to_s,
+        "rank" => @rank.to_s,
         "created_at" => @created_at.to_s,
         "deleted" => @deleted.to_s,
     }.to_json
