aa9e2e0ebdbe6e38329989814b339e5d331d314a
Charles Cloud <charliecloud@gmail.com>
2018-03-25 14:19:09 -0400

    More updates to location code, add deleted to db model for salary

--- models/salary.rb
+++ models/salary.rb
@@ -6,6 +6,7 @@ class Salary
   property :count, Integer
   property :created_at, DateTime
   property :updated_at, DateTime
+  property :deleted, Boolean, :default => false
 
   belongs_to :location
   belongs_to :position
